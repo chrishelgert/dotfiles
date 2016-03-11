66c6e1364e1b503c05e772c881095943d96325f0
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-03-11 20:35:25 +0100

    homebrew

---
+++ homebrew/brew-cask.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install native apps
+brew install caskroom/cask/brew-cask
+brew tap caskroom/versions
+
+# daily
+brew cask install spectacle
+brew cask install rescuetime
+brew cast install flux
+
+# dev
+brew cask install iterm2
+brew cask install atom
+
+# different things
+brew cask install vlc
+brew cask install gpgtools
---
+++ homebrew/brew.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Install tools via homebrew
+brew update
+brew upgrade
+
+# GNU core utilities (those that come with OS X are outdated)
+brew install coreutils
+brew install moreutils
+brew install findutils
+brew install gnu-sed --default-names
+
+# regular bash-completion package is held back to an older release,
+# so we get latest from versions.
+brew tap homebrew/versions
+brew install homebrew/versions/bash-completion2
+brew install homebrew/completions/brew-cask-completion
+
+# generic colouriser
+# http://kassiopeia.juls.savba.sk/~garabik/software/grc/
+brew install grc
+
+# Install wget with IRI support
+brew install wget --enable-iri
+
+# Install more recent versions of some OS X tools
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/screen
+
+# mtr - ping & traceroute. best.
+brew install mtr
+    # allow mtr to run without sudo
+    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//')
+    sudo chmod 4755 $mtrlocation/sbin/mtr
+    sudo chown root $mtrlocation/sbin/mtr
+
+
+# Install other useful binaries
+brew install pv
+brew install rename
+brew install tree
+
+# Notifier
+brew install terminal-notifier
+
+# colored logcat guy
+brew install pidcat
+
+# best shell
+brew install zsh
+
+# Devtools
+brew install git
+brew install node
+brew install mongodb
+brew install go
+
+# Remove outdated versions from the cellar
+brew cleanup
