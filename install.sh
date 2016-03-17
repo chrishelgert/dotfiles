8d0a10510a593f01a977b2fea7f86004e8c08462
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-03-17 20:34:24 +0100

    add all components to install.sh

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Install all tools
+## Homebrew
+./homebrew/brew.sh
+./homebrew/brew-cask.sh
+
+## Node.js
+./node/install.sh
+
+## Oh my zsh
+./shell/zsh.sh
+
+# Git
+./git/repos.sh
+
+## for the c alias (syntax highlighted cat)
+sudo easy_install Pygments
+
+## set up osx defaults
+sh .osx
+
+## load symlinks
+./symlink/symlink-setup.sh
