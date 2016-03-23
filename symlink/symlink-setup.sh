b457ebd61ece7ec0fe40b35966bc01520df15418
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-03-23 18:33:48 +0100

    fixed symlink-name

--- symlink/symlink-setup.sh
+++ symlink/symlink-setup.sh
@@ -124,16 +124,6 @@ print_success() {
     printf "\e[0;32m  [✔] $1\e[0m\n"
 }
 
-
-
-
-
-
-#
-# actual symlink stuff
-#
-
-
 # finds all .dotfiles in this folder
 declare -a FILES_TO_SYMLINK=$(find . -type f -maxdepth 1 -name ".*" -not -name .DS_Store -not -name .git -not -name .osx | sed -e 's|//|/|' | sed -e 's|./.|.|')
 FILES_TO_SYMLINK="$FILES_TO_SYMLINK .vim bin" # add in vim and the binaries
