1796215ef8929d4dc4030f3e76adab6998439655
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-03-23 18:56:29 +0100

    fixed zsh.sh

--- shell/zsh.sh
+++ shell/zsh.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo '/usr/local/bin/zsh' >> /etc/shells
+mkdir ~/code/antigen
 curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/code/antigen/antigen.zsh
