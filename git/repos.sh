c1a985b19a96d39b4da4ffbe1bd78daa026af4ec
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-04-14 17:09:15 +0200

    added git standup

--- git/repos.sh
+++ git/repos.sh
@@ -2,3 +2,7 @@
 
 # disable itunes opening on media keys
 git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
+
+git clone https://github.com/kamranahmedse/git-standup.git ~/code/git-standup
+cd ~/git-standup
+sudo make install
