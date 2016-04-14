3b51367574563b958936c1c6e71fcb8fbfeb83e2
Chris Helgert <chris.helgert@webdeveloperco.de>
2016-04-14 17:10:21 +0200

    fixed path for git-standup

--- git/repos.sh
+++ git/repos.sh
@@ -4,5 +4,5 @@
 git clone https://github.com/thebitguru/play-button-itunes-patch ~/code/play-button-itunes-patch
 
 git clone https://github.com/kamranahmedse/git-standup.git ~/code/git-standup
-cd ~/git-standup
+cd ~/code/git-standup
 sudo make install
