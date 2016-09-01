9dfc4877135790c6777b9979980a89a5af306f69
Chris Helgert <chris.helgert@yahoo.com>
2016-09-01 19:08:33 +0200

    updated nvm-version in install-script

--- node/install.sh
+++ node/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.6/install.sh | bash
 
 # Install node via nvm
 nvm install node
