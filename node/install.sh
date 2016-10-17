54a5c09166b54d55609c824c841bb15a3e570570
Chris Helgert <chris.helgert@yahoo.com>
2016-10-17 14:37:10 +0200

    issue #2 add vtop

--- node/install.sh
+++ node/install.sh
@@ -7,4 +7,4 @@ nvm install node
 curl -o- -L https://yarnpkg.com/install.sh | bash
 
 ## Install mostly used npm-packages
-sudo npm install --global speed-test git-open diff-so-fancy pm2 npm-check greenkeeper now create-react-app yo
+sudo npm install --global speed-test git-open diff-so-fancy pm2 npm-check greenkeeper now create-react-app yo vtop
