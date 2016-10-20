41af062430d9163cec02cb1502d1804ca36073a2
Chris Helgert <chris.helgert@yahoo.com>
2016-10-20 10:51:24 +0200

    replace yarn-installation with homebrew-one

--- node/install.sh
+++ node/install.sh
@@ -3,8 +3,5 @@
 # Install node via nvm
 nvm install node
 
-# install yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash
-
 ## Install mostly used npm-packages
 sudo npm install --global speed-test git-open diff-so-fancy pm2 npm-check greenkeeper now create-react-app yo vtop
