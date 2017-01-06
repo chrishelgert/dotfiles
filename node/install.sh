45024086ea18e4e961045e7a0acc4168dac299be
Chris Helgert <chrishelgert@users.noreply.github.com>
2017-01-06 17:52:44 +0100

    fixed typo - should be np instead of ncp

--- node/install.sh
+++ node/install.sh
@@ -3,4 +3,4 @@
 ## Install mostly used npm-packages
 yarn global add n speed-test \
   git-open diff-so-fancy pm2 npm-check \
-  now create-react-app yo vtop ncp release
+  now create-react-app yo vtop np release
