bbc8139fe62823298dd41b530836057622659960
Chris Helgert <chris.helgert@online.de>
2017-01-23 17:19:14 +0100

    add zeit/micro

--- node/install.sh
+++ node/install.sh
@@ -3,4 +3,5 @@
 ## Install mostly used npm-packages
 yarn global add n speed-test \
   git-open diff-so-fancy pm2 npm-check \
-  now create-react-app yo vtop np release
+  now create-react-app yo vtop np release \
+  micro
