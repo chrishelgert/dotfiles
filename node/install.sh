4b28a55c8c18fb8cb2733297bb2dc364fe84e55d
Chris Helgert <chris.helgert@online.de>
2017-03-12 13:22:51 +0100

    add react-native-cli

--- node/install.sh
+++ node/install.sh
@@ -2,6 +2,6 @@
 
 ## Install mostly used npm-packages
 yarn global add n speed-test \
-  git-open diff-so-fancy pm2 npm-check \
-  now create-react-app yo vtop np release \
-  micro
+  git-open diff-so-fancy npm-check \
+  create-react-app react-native-cli vtop \
+  yo np now micro release
