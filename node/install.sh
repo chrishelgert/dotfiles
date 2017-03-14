eaf0c125517f5d87515df7ec86a17f8f6c2f0e70
Chris Helgert <chris.helgert@online.de>
2017-03-14 18:07:08 +0100

    add create-react-native-app

--- node/install.sh
+++ node/install.sh
@@ -3,5 +3,5 @@
 ## Install mostly used npm-packages
 yarn global add n speed-test \
   git-open diff-so-fancy npm-check \
-  create-react-app react-native-cli vtop \
-  yo np now micro release
+  create-react-app react-native-cli create-react-native-app \
+  vtop yo np now micro release
