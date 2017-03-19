197d4499dbfc37302ec2bc13b8b29423151289c9
Chris Helgert <chris.helgert@online.de>
2017-03-19 13:15:54 +0100

    add clean install for global yarn, after node-version has changed

--- node/install.sh
+++ node/install.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+package=(
+  n
+  yo
+  np
+  now
+  vtop
+  micro
+  release
+  git-open
+  speed-test
+  diff-so-fancy
+  react-native-cli
+  create-react-app
+  create-react-native-app
+)
+
 ## Install mostly used npm-packages
-yarn global add n speed-test \
-  git-open diff-so-fancy npm-check \
-  create-react-app react-native-cli create-react-native-app \
-  vtop yo np now micro release
+## remove them if they already exists
+for i in ${package[@]}; do
+  yarn global remove $i
+  yarn global add $i
+done
--- update.sh
+++ update.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+# store node-version
+nodeVersion="$(node -v)"
+
 # Homebrew and Cask
 brew update
 brew upgrade
 brew cleanup
 brew cask cleanup
 
-# App Store
+# # App Store
 mas upgrade
 
 # Node
-yarn global upgrade
+# if node-version has change, remove and install the global yarn packages
+if [ "$(node -v)" != $nodeVersion ]; then
+  sh ./node/install.sh
+else
+  yarn global upgrade
+fi
