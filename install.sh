c1ef82a43ea46528de17a7840c48550fd994c741
Chris Helgert <chrishelgert@users.noreply.github.com>
2017-05-31 20:48:16 +0200

    Webassembly with rust (#17)
    * first steps with rust and webassembly
    
    * add webassembly installation
    
    * add emscripten to path and set required variables
    
    * remove autogenerated .emscripten config

--- install.sh
+++ install.sh
@@ -24,7 +24,13 @@ sh ./shell/zsh.sh
 sh ./git/repos.sh
 
 # Gems
-sh ./gems/isntall.sh
+sh ./gems/install.sh
+
+# Rust
+sh ./rust/install.sh
+
+# Webassembly
+sh ./webassembly/install.sh
 
 ## for the c alias (syntax highlighted cat)
 sudo easy_install Pygment
