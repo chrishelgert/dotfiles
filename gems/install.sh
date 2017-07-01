4cdbb59dedb5b5a1e5ff08474fa830682f212ba8
Chris Helgert <chris.helgert@online.de>
2017-07-01 10:04:41 +0200

    cleanup gems and webassembly with rust

--- gems/install.sh
+++
@@ -1,2 +0,0 @@
-# install travisci-cli
-sudo gem install travis -v 1.8.5 --no-rdoc --no-ri
--- install.sh
+++ install.sh
@@ -23,15 +23,6 @@ sh ./shell/zsh.sh
 ## Git
 sh ./git/repos.sh
 
-# Gems
-sh ./gems/install.sh
-
-# Rust
-sh ./rust/install.sh
-
-# Webassembly
-sh ./webassembly/install.sh
-
 ## for the c alias (syntax highlighted cat)
 sudo easy_install Pygment
 
@@ -40,3 +31,6 @@ sh ./symlink/symlink-setup.sh
 
 ## Use Zsh as defaultshell
 chsh -s /usr/local/bin/zsh
+
+## Install go after symlinks and zsh for right GOPATH
+sh ./golang/install.sh
