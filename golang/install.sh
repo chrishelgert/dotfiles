d16958f97730f68737f6050bdbef2c0a097edebc
Chris Helgert <chris.helgert@online.de>
2017-08-11 14:43:29 +0200

    add go-wrk and go-torch

--- golang/install.sh
+++ golang/install.sh
@@ -6,3 +6,9 @@ go get -u github.com/tj/mmake/cmd/mmake
 
 # Goviz
 go get -u github.com/hirokidaichi/goviz
+
+# a small heavy duty http/https benchmark tool written in go
+go get -u github.com/adjust/go-wrk
+
+# Stochastic flame graph profiler for Go programs
+go get -u github.com/uber/go-torch
