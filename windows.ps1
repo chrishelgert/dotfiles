47bcee8c111850d84e011c86f0a1a1da48526843
Chris Helgert <chrishelgert@users.noreply.github.com>
2018-08-31 10:05:25 +0200

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -67,6 +67,10 @@ Write-Host "> All windows programs have been installed."
 
 ### Finish
 
-Write-Host "> Restart your PC for further WSL installation."
-Write-Host "> After restarting, download your favorit Linux distribution from the Microsoft Store."
-Write-Host "> Visit https://docs.microsoft.com/en-us/windows/wsl/install-win10#install-your-linux-distribution-of-choice  for more informations"
+if ($wsl eq "y") {
+  Write-Host "> Restart your PC for further WSL installation."
+  Write-Host "> After restarting, download your favorit Linux distribution from the Microsoft Store."
+  Write-Host "> Visit https://docs.microsoft.com/en-us/windows/wsl/install-win10#install-your-linux-distribution-of-choice  for more informations"
+} else {
+  Write-Host "> Finished windows setup."
+}
