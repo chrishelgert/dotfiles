9ddec99fa8cd52eaab2f7279ca24fad9bfac8c56
Chris Helgert <chris.helgert@online.de>
2018-09-03 12:13:09 +0200

    Add yarn to install script

--- unix.sh
+++ unix.sh
@@ -7,13 +7,17 @@ apt-get update
 apt-get install git-core
 
 ## install build tools
-
 sudo apt-get install cmake
 
 ### node.js
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo apt-get install -y build-essential
+
+### yarn (node.js package manager)
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
 
 ### golang
 sudo add-apt-repository ppa:gophers/archive
