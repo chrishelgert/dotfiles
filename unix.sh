a2b49bbccca452a6fdded8b4b42c9fc1f780b6cd
Chris Helgert <chrishelgert@users.noreply.github.com>
2018-10-10 10:04:00 +0400

    Add nerd-fonts and change default to fira code (#23)
    * Add nerd-fonts
    
    * Change font to fira code

--- unix.sh
+++ unix.sh
@@ -79,18 +79,27 @@ sudo chmod -R 755 ~/.zplug
 #TODO: Ask for windows home directory and use it
 username="chris"
 mkdir ~/workspace && cd "$_"
+ln -s /mnt/c/Users/$username/workspace ~/workspace
+
 git clone https://github.com/chrishelgert/dotfiles
 cd dotfiles
 
 ### Symlink dotfiles
 
 sudo rm -rf /etc/wsl.conf && sudo ln -s ~/workspace/dotfiles/shell/wsl.conf /etc/wsl.conf
-#ln -s /mnt/c/Users/$username/workspace ~/workspace
 rm -f ~/.bashrc && ln -s ~/workspace/dotfiles/shell/.bashrc ~/.bashrc
 rm -f ~/.zshenv && ln -s ~/workspace/dotfiles/shell/.zshenv ~/.zshenv
 rm -f ~/.zshrc && ln -s ~/workspace/dotfiles/shell/.zshrc ~/.zshrc
 rm -rf ~/.gitconfig && ln -s ~/workspace/dotfiles/shell/.gitconfig ~/.gitconfig
 rm -rf ~/.config/nvim/init.vim && mkdir -p ~/.config/nvim && ln -s ~/workspace/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+cd ..
+
+### Load fonts
+
+git clone https://github.com/ryanoasis/nerd-fonts --depth 1
+cd nerd-fonts
+bash ./install.sh FiraCode
+cd ..
 
 ## Cleanup
 
