a0117543b66efb0fb07d25bd42f5a7209b95e18c
Chris Helgert <chris.helgert@online.de>
2018-10-27 21:18:16 +0200

    Add shortcut for starting i3 from desktop

--- windowsAfterWSL.ps1
+++ windowsAfterWSL.ps1
@@ -9,3 +9,12 @@ New-Item -Path C:\Users\$($env:UserName)\.hyper.js -ItemType SymbolicLink -Value
 
 Add-MpPreference -ExclusionPath C:\Users\$($env:UserName)\workspace
 App-MpPreference -ExclusionPath C:\Users\$($env:UserName)\AppData\Local\Packages\CanonicalGroupLimited.UbuntuonWindows_79rhkp1fndgsc\LocalState\rootfs
+
+# Add shortcut for wsl start file
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$Home\Desktop\i3.lnk")
+$Shortcut.TargetPath = "C:\Tools\i3\i3.vbs"
+$shortcut.IconLocation="C:\Tools\i3\icon.ico"
+$shortcut.WorkingDirectory="C:\Tools\i3"
+$Shortcut.Save()
