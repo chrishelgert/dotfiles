f95c92cc1f356f779271562b57e53c57d52c65cb
Chris Helgert <chris.helgert@online.de>
2018-11-12 14:00:33 +0100

    Add spotify

--- unix.sh
+++ unix.sh
@@ -119,6 +119,15 @@ sudo apt-get install zlib1g-dev # required for building bat
 cargo install bat               # cat with colors
 cargo install exa               # better ls
 
+
+## Different things
+
+### Spotify
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update
+sudo apt-get install spotify-client
+
 ## shell
 
 ### zsh
