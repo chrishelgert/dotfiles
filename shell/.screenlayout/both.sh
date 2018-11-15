99c1f66ccb6b3bc16d3908ef8d96065510cc72c6
Chris Helgert <chris.helgert@online.de>
2018-11-15 09:08:01 +0100

    Update screenlayout and fix dpi

---
+++ shell/.screenlayout/both.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-2-1 --off --output DP-2-2 --primary --mode 1920x1200 --pos 0x600 --rotate normal --output DP-2-3 --off --output eDP-1 --mode 3840x2160 --pos 1920x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
--- shell/.screens/docked.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI-1 --off \
-  --output DP-2-2 --primary --mode 1920x1080 --pos 0x0
