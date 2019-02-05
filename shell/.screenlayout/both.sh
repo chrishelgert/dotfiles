00e0f96db19d0c850ff63e12357e69d5f468816c
Chris Helgert <chris.helgert@online.de>
2019-02-05 15:02:12 +0100

    Optimize screenlayouts

--- shell/.screenlayout/both.sh
+++ shell/.screenlayout/both.sh
@@ -1,11 +1,4 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off \
-  --output DP-2-2 --primary --mode 1920x1200 --pos 0x600 --rotate normal --scale 2x2 \
-  --output DP-2-3 --off \
-  --output eDP-1 --mode 3840x2160 --pos 3840x0 --rotate normal \
-  --output HDMI-2 --off \
-  --output HDMI-1 --off \
-  --output DP-2 --off \
-  --output DP-1 --off
+xrandr --output DP-2-1 --off --output DP-2-2 --primary --mode 1920x1200 --pos 0x152 --scale 2x2 --rotate normal --output DP-2-3 --off --output eDP-1 --mode 3840x2160 --pos 3840x392 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
 
 i3-msg restart
