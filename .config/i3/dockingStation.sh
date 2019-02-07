1230dee2204ce4258e50f3e253dfdfb3c227e6da
Chris Helgert <chris.helgert@online.de>
2019-02-07 20:15:12 +0100

    Use one script to change the screen layout via i3 keybinding

--- .config/i3/dockingStation.sh
+++ .config/i3/dockingStation.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
 xrandr --output DP-2-1 --off --output DP-2-2 --primary --mode 1920x1200 --pos 0x152 --scale 2x2 --rotate normal --output DP-2-3 --off --output eDP-1 --mode 3840x2160 --pos 3840x392 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
-
-i3-msg restart
