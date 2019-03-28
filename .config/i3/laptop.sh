0a51092ed6eb9767c63212ca763b810137c11524
Chris Helgert <chris.helgert@online.de>
2019-03-28 08:02:26 +0100

    fix: update of driver changed output names

--- .config/i3/laptop.sh
+++ .config/i3/laptop.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-2-1 --off --output DP-2-2 --off --output DP-2-3 --off --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off
+xrandr --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output eDP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal --output HDMI-2 --off --output HDMI-1 --off --output DP-2 --off --output DP-1 --off --output DVI-I-1-1 --off --output DVI-I-2-2 --off
--- .config/i3/toggleScreens.sh
+++ .config/i3/toggleScreens.sh
@@ -4,7 +4,7 @@
 connectedScreens=$(xrandr | grep " connected " | awk '{ print$1 }')
 
 # DP2-2 is the screen on the docking station
-if [[ $connectedScreens == *"DP-2-2" ]];then
+if [[ $connectedScreens == *"DP2-2" ]];then
   # Docking station mode
   sh "$HOME/.config/i3/dockingStation.sh"
 elif [[ $connectedScreens == *"DVI-I-" ]];then
