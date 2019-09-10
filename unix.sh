8b45b5dcb52fced0bcf07880f2ec4cdb1a11f02f
Chris Helgert <chrishelgert@users.noreply.github.com>
2019-09-10 08:23:32 +0200

    Update installation of spotify

--- unix.sh
+++ unix.sh
@@ -299,8 +299,8 @@ function install_polybar {
 }
 
 function install_spotify {
-  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-  echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+  url -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+  echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
   sudo apt update
   sudo apt install spotify-client
 }
