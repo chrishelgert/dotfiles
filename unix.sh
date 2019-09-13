28be49bed48763cec710a1f5e2ad4671688aa665
Chris Helgert <chrishelgert@users.noreply.github.com>
2019-09-13 09:28:59 +0200

    fix(typo): curl instead of url

--- unix.sh
+++ unix.sh
@@ -299,7 +299,7 @@ function install_polybar {
 }
 
 function install_spotify {
-  url -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+  curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
   sudo apt update
   sudo apt install spotify-client
