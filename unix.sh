0e4e7841f599e022f756a2e13ad4872d0059cfa2
Chris Helgert <chrishelgert@users.noreply.github.com>
2020-03-04 18:57:03 +0100

    fix: install for polybar

--- unix.sh
+++ unix.sh
@@ -151,7 +151,7 @@ function install_wm {
     xdotool
 
   # polybar
-  sudo pt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev \
+  sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev \
     libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen \
     xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
 
