c2ef0a58bd394ee5086290de60cc7c9f12b64775
Chris Helgert <chris.helgert@online.de>
2020-05-04 10:15:15 +0200

    chore(tools): add rakyll/hey

--- unix.sh
+++ unix.sh
@@ -109,7 +109,8 @@ function install_golang {
   go get -u github.com/genuinetools/weather
   go get -u github.com/guillaumebreton/gone
   go get -u github.com/cjbassi/gotop
-  go get -u github.com/golang/dep/cmd/dep
+  go get -u github.com/golang/dep/cmd/dep]
+  go get -u github.com/rakyll/hey
 }
 
 function install_rust {
