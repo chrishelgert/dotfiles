d2ba11d47444001d06924ff4ee2491f4f69a448a
Chris Helgert <chrishelgert@users.noreply.github.com>
2020-05-17 17:53:05 +0200

    chore: update node to latest version

--- unix.sh
+++ unix.sh
@@ -69,7 +69,7 @@ function install_zsh {
 
 # Installs node.js and yarn
 function install_nodeJS {
-  curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+  curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
   echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 
