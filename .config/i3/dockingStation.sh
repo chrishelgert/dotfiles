5e8a21ec7ec8266528c769cbdeaddaa58b7132e6
Chris Helgert <chris.helgert@online.de>
2020-11-13 14:35:18 +0100

    chore: drop no longer used configs

--- .config/i3/dockingStation.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 3840x2160 --pos 3840x192 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --mode 1920x1200 --pos 0x0 --rotate normal --scale 2x2 --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
